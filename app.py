ba9fff384dd61a5a3d98d515b568cb61b66b0515
andrewkrug <andrewkrug@gmail.com>
2017-02-07 15:40:27 -0800

    add cache buster asset pipeline and templates

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+
+
 app = Flask(__name__)
+
+assets = Environment(app)
+
+js = Bundle('js/base.js',
+            filters='jsmin', output='js/gen/packed.js')
+assets.register('js_all', js)
+
+
+sass = Bundle('*.sass', filters='sass', output='gen/sass.css')
+all_css = Bundle('css/jquery.calendar.css', sass,
+                 filters='cssmin', output="gen/all.css")
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()

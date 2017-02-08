6a11ac03dee87f43dc2d41c0b4bba2c68e769023
andrewkrug <andrewkrug@gmail.com>
2017-02-07 16:55:20 -0800

    pep8 test and update gitignore

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python
-import os
-import pytest
+"""Route decorator test ."""
+
+
 from dashboard import app as sso_dashboard
 
+
 class TestDashboardRoutes(object):
+    """Dashboard client instantiation object."""
+
     def __init__(self):
+        """Will return a flask testing client for route testing."""
         self.app = self.create_app()
 
     def create_app(self):
+        """Set any additional test time config params here."""
         sso_dashboard.app.config['TESTING'] = True
         return sso_dashboard.app.test_client()
 
+
 CLIENT = TestDashboardRoutes()
 
+
 def test_index():
+    """Validate the resulting body of the index page."""
     result = CLIENT.app.get('/')
     assert b'HelloWorld' in result.data

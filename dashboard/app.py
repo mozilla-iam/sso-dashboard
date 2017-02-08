48261f236c09f92e5b2743f61b6f117aa4a77de1
andrewkrug <andrewkrug@gmail.com>
2017-02-08 11:00:39 -0800

    dotenv loading support for testing configuration vars

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
+from dotenv import load_dotenv, find_dotenv
+from os.path import join, dirname
+import os
 
+import config
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
+
+if os.environ.get('ENVIRONMENT') == 'Production':
+    app.config.from_object(config.ProductionConfig())
+elif os.environ.get('ENVIRONMENT') == 'Development':
+    print("Development Mode Running")
+    app.config.from_object(config.DevelopmentConfig())
 
 assets = Environment(app)
 
---
+++ dashboard/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""Class that governs all authentication with open id connect."""
+
+
+class Auth(object):
+    """Auth object for login, logout, and response validation."""
+
+    def __init__(self):
+        """Object initializer for auth object."""
+        pass

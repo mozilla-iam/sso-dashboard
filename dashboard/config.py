95802bc4f21fb2b202cf316c15a96224a431c5ee
andrewkrug <andrewkrug@gmail.com>
2017-02-08 13:56:59 -0800

    redirect to hosted lock on no session

--- dashboard/config.py
+++ dashboard/config.py
@@ -14,6 +14,10 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
 
+    SERVER_NAME = os.environ['SERVER_NAME']
+    PERMANENT_SESSION = os.environ['PERMANENT_SESSION']
+    PERMANENT_SESSION_LIFETIME = os.environ['PERMANENT_SESSION_LIFETIME']
+
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -31,3 +35,33 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+
+
+class OIDCConfig(object):
+    """Convienience Object for returning required vars to flask."""
+    def __init__(self):
+        """General object initializer."""
+        self.OIDC_DOMAIN = os.environ['OIDC_DOMAIN']
+        self.OIDC_CLIENT_ID = os.environ['OIDC_CLIENT_ID']
+        self.OIDC_CLIENT_SECRET = os.environ['OIDC_CLIENT_SECRET']
+        self.LOGIN_URL = "https://{DOMAIN}/login?client={CLIENT_ID}".format(
+            DOMAIN=self.OIDC_DOMAIN,
+            CLIENT_ID=self.OIDC_CLIENT_ID
+        )
+
+
+    def auth_endpoint(self):
+        return "https://{DOMAIN}/authorize".format(
+            DOMAIN=self.OIDC_DOMAIN
+        )
+
+    def token_endpoint(self):
+        return "https://{DOMAIN}/oauth/token".format(
+                DOMAIN=self.OIDC_DOMAIN
+        )
+
+    def client_id(self):
+        return self.OIDC_CLIENT_ID
+
+    def client_secret(self):
+        return self.OIDC_CLIENT_SECRET

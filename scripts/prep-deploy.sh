2639ce6850844a0214a6e01a3875cd4c8850d5b9
andrewkrug <andrewkrug@gmail.com>
2017-04-17 15:46:32 -0700

    update copy script for supervisord

--- scripts/prep-deploy.sh
+++ scripts/prep-deploy.sh
@@ -2,6 +2,6 @@
 
 /etc/init.d/supervisord stop
 
-cp /home/ec2-user/app/conf/supervisor-ansible.conf /etc/supervisord.d/
+cp /home/ec2-user/app/conf/supervisor-ansible.ini /etc/supervisord.d/
 
 rm -rf /home/ec2-user/app/*

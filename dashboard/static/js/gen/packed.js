4b3c1cadb9b880c0e71a524d9a0bdeaf1deb3822
Nikos Roussos <nikos@roussos.cc>
2017-04-21 13:22:48 +0300

    Remove app logos and generated assets

--- dashboard/static/js/gen/packed.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){$("[data-toggle~=collapse]").click(function(){$(".opacity").toggle();});});$(':input[name=filter]').on('input',function(){var val=this.value.toLowerCase();$('#app-grid').find('.app-tile')
-.filter(function(){return $(this).data('id').toLowerCase().indexOf(val)>-1;})
-.show()
-.end().filter(':visible')
-.filter(function(){return $(this).data('id').toLowerCase().indexOf(val)===-1;})
-.fadeOut();});

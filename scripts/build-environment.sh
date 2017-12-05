7290068c24f92f7ed6a29fa13858d7c0ad4eae3d
andrewkrug <andrewkrug@gmail.com>
2017-12-05 14:17:20 -0800

    update stack and deploy scripts

--- scripts/build-environment.sh
+++ scripts/build-environment.sh
@@ -9,6 +9,6 @@ ACCOUNT_ID=`aws sts get-caller-identity | grep Account | cut -d '"' -f4`
 
 sed -i s/656532927350/${ACCOUNT_ID}/g docker-compose.yml
 
-aws ecr get-login --region us-west-2 | bash
+aws ecr get-login --region us-west-2 --no-include-email | bash
 
 /usr/local/bin/docker-compose pull

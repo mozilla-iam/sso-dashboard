42e2a3d7d3cb76eb03cc2ba4411eaed8d3113b13
andrewkrug <andrewkrug@gmail.com>
2017-12-06 10:27:16 -0800

    fix indexing of keys python 2=>3 in vanity router

--- dashboard/vanity.py
+++ dashboard/vanity.py
@@ -12,21 +12,23 @@ class Router(object):
 
     def setup(self):
         for url in self.url_list:
-            try:
-                self.app.add_url_rule(url.keys()[0], url.keys()[0], self.redirect_url)
-                self.app.add_url_rule(url.keys()[0] + "/", url.keys()[0] + "/", self.redirect_url)
-            except Exception as e:
-                print(e)
+            for vanity_url in url.keys():
+                try:
+                    self.app.add_url_rule(vanity_url, vanity_url, self.redirect_url)
+                    self.app.add_url_rule(vanity_url + "/", vanity_url + "/", self.redirect_url)
+                except Exception as e:
+                    print(e)
 
     def redirect_url(self):
         vanity_url = '/' + request.url.split('/')[3]
 
         for match in self.url_list:
-            if match.keys()[0] == vanity_url:
-                resp = make_response(redirect(match[vanity_url], code=301))
-                resp.headers['Cache-Control'] = ('no-store, no-cache, must-revalidate, '
-                                                 'post-check=0, pre-check=0, max-age=0')
-                resp.headers['Expires'] = '-1'
-                return resp
-            else:
-                pass
+            for key in match.keys():
+                if key == vanity_url:
+                    resp = make_response(redirect(match[vanity_url], code=301))
+                    resp.headers['Cache-Control'] = ('no-store, no-cache, must-revalidate, '
+                                                     'post-check=0, pre-check=0, max-age=0')
+                    resp.headers['Expires'] = '-1'
+                    return resp
+                else:
+                    pass

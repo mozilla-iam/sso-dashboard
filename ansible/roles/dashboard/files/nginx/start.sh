d1e945d8e943d712fe315625bc7d8fd6431feae8
andrewkrug <andrewkrug@gmail.com>
2017-12-06 08:57:52 -0800

    force python3

--- ansible/roles/dashboard/files/nginx/start.sh
+++ ansible/roles/dashboard/files/nginx/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gunicorn app:app --worker-class gevent --bind 0.0.0.0:8000 --reload &
+python3 -m gunicorn.app.wsgiapp app:app --worker-class gevent --bind 0.0.0.0:8000 --reload &
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"

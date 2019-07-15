38967d4d86ed5a2e7310497abc0fe7c0e5216c31
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-15 15:23:44 -0400

    Create test client

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,20 @@ from dashboard.app import app
 from flask import url_for
 
 
+@pytest.fixture
+def client():
+    db_fd, dashboard.app.config['DATABASE'] = tempfile.mkstemp()
+    dashboard.app.config['TESTING'] = True
+    client = dashboard.app.test_client()
+
+    with dashboard.app.app_context():
+        dashboard.init_db()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(dashboard.app.config['DATABASE'])
+
+
 def test_root(client):
-    assert client.get(url_for('/')).status_code == 302
+    assert client.get('/').status_code() == 302

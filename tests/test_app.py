b085df7eceb5ae94d98f873366fbb6ac2e5e4e4b
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-15 15:19:27 -0400

    Simplify

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,5 @@ from dashboard.app import app
 from flask import url_for
 
 
-@pytest.fixture
-def app():
-    app = create_app()
-    return app
-
-
 def test_root(client):
     assert client.get(url_for('/')).status_code == 302

198dafa9ca8b57118f1e78b7ec16d20e806252f6
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-15 14:49:37 -0400

    Add skeleton for basic flask route testing

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import os
+import tempfile
+import pytest
+from dashboard.app import app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+
+def test_root(client):
+    res = client.get(url_for('/'))
+    assert res.status_code == 200

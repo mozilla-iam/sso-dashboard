66c051b546acbfe2594568fc3e37c0651222e753
Felicia Rosemond <frosemond@gmail.com>
2024-02-02 17:08:36 -0500

    IAM-1256 re-implemented changed for IAM-1256 into a new branch

--- dashboard/models/tile.py
+++ dashboard/models/tile.py
@@ -2,36 +2,30 @@
 import boto3
 import logging
 import os
-
+import urllib3
 
 from boto3.dynamodb.conditions import Attr
 
-
 logger = logging.getLogger(__name__)
 
-
-class S3Transfer(object):
-    """News up and does the job if configuration specifies S3 for data transfer."""
-
+class CDNTransfer(object):
+    """Download apps.yaml from CDN"""
     def __init__(self, app_config):
         self.app_config = app_config
-        self.client = None
-        self.s3_bucket = self.app_config.S3_BUCKET
+        """Used in app.py load apps.yml"""
         self.apps_yml = None
-
-    def connect_s3(self):
-        if not self.client:
-            self.client = boto3.client("s3")
+        self.url = self.app_config.CDN + "/apps.yml"
 
     def is_updated(self):
-        """Compare etag of what is in bucket to what is on disk."""
-        self.connect_s3()
-        try:
-            self.client.head_object(Bucket=self.s3_bucket, Key="apps.yml", IfMatch=self._etag())
-            return False
-        except Exception as e:
-            logger.error("Etags do not match as a result of {error}".format(error=e))
+        """Compare etag of what is in CDN to what is on disk."""
+        http = urllib3.PoolManager()
+        response = http.request('HEAD', self.url)
+
+        if (response.headers["ETag"] != self._etag()):
+            logger.error("Etags do not match")
             return True
+        else:
+            return False
 
     def last_update(self):
         this_dir = os.path.dirname(__file__)
@@ -39,6 +33,7 @@ class S3Transfer(object):
         return abs(os.path.getmtime(filename))
 
     def _update_etag(self, etag):
+        """Update the etag file."""
         this_dir = os.path.dirname(__file__)
         filename = os.path.join(this_dir, "../data/{name}").format(name="apps.yml-etag")
         c = open(filename, "w+")
@@ -46,28 +41,26 @@ class S3Transfer(object):
         c.close()
 
     def _etag(self):
+        """get the etag from the file"""
         this_dir = os.path.dirname(__file__)
         filename = os.path.join(this_dir, "../data/{name}").format(name="apps.yml-etag")
         try:
             return open(filename, "r").read()
         except Exception as e:
-            logger.error("Error fetching etag: {e}".format(e=e))
+            """If the etag file is not found return a default etag."""
+            logger.info("Error fetching etag: {e}".format(e=e))
             return "12345678"
 
     def _get_config(self):
-        self.connect_s3()
-        apps_yml = self.client.get_object(Bucket=self.s3_bucket, Key="apps.yml")
-
-        response = self.client.head_object(Bucket=self.s3_bucket, Key="apps.yml")
-
-        self.apps_yml = apps_yml.get("Body").read().decode("utf-8")
-
+        """Download the apps.yml from the CDN."""
+        http = urllib3.PoolManager()
+        response = http.request('GET', self.url)
         this_dir = os.path.dirname(__file__)
         filename = os.path.join(this_dir, "../data/{name}").format(name="apps.yml")
-        c = open(filename, "w+")
-        c.write(self.apps_yml)
-        c.close()
-        self._update_etag(response.get("ETag"))
+        with open(filename, 'wb') as file:
+            file.write(response.data)
+        self.apps_yml = response.data.decode("utf-8")
+        self._update_etag(response.headers["ETag"])
 
     def _touch(self):
         fname = "dashboard/app.py"
@@ -78,7 +71,7 @@ class S3Transfer(object):
             fhandle.close()
 
     def sync_config(self):
-        self.connect_s3()
+        """Determines if the config file is updated and if so fetches the new config."""
         try:
             if self.is_updated():
                 logger.info("Config file is updated fetching new config.")
@@ -86,32 +79,11 @@ class S3Transfer(object):
                 # Touch app.py to force a gunicorn reload
                 self._touch()
                 return True
-            else:
-                self._get_config()
-                return False
-                # Do nothing
         except Exception as e:
             print(e)
             logger.error("Problem fetching config file {error}".format(error=e))
 
-
-class DynamoTransfer(object):
-    """News up and does the job if configuration specifies that dynamo should be used for app information."""
-
-    def __init__(self, app_config):
-        self.configuration_table_name = "sso-dashboard-apps"
-        self.dynamodb = None
-
-    def connect_dynamodb(self):
-        if self.dynamodb is None:
-            dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
-            table = dynamodb.Table(self.configuration_table_name)
-            self.dynamodb = table
-
-    def sync_config(self):
-        self.connect_dynamodb()
-        results = self.dynamodb.scan(FilterExpression=Attr("name").exists())
-        return results.get("Items", None)
+        return False # Do nothing
 
 
 class Tile(object):

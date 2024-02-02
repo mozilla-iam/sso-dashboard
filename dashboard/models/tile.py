6755c258c5f356d068e397c98d89056edb543529
Felicia Rosemond <frosemond@gmail.com>
2024-02-02 08:53:19 -0500

    IAM-1256 removed DynmanTransfer since it is not used

--- dashboard/models/tile.py
+++ dashboard/models/tile.py
@@ -9,20 +9,19 @@ from boto3.dynamodb.conditions import Attr
 logger = logging.getLogger(__name__)
 
 class CDNTransfer(object):
-    """Download app.yaml from CDN"""
+    """Download apps.yaml from CDN"""
     def __init__(self, app_config):
         self.app_config = app_config
+        """Used in app.py load apps.yml"""
         self.apps_yml = None
         self.url = self.app_config.CDN + "/apps.yml"
 
     def is_updated(self):
         """Compare etag of what is in CDN to what is on disk."""
-
         http = urllib3.PoolManager()
         response = http.request('HEAD', self.url)
-        lastestEtag = response.headers["ETag"]
 
-        if (lastestEtag != self._etag()):
+        if (response.headers["ETag"] != self._etag()):
             logger.error("Etags do not match")
             return True
         else:
@@ -34,6 +33,7 @@ class CDNTransfer(object):
         return abs(os.path.getmtime(filename))
 
     def _update_etag(self, etag):
+        """Update the etag file."""
         this_dir = os.path.dirname(__file__)
         filename = os.path.join(this_dir, "../data/{name}").format(name="apps.yml-etag")
         c = open(filename, "w+")
@@ -52,6 +52,7 @@ class CDNTransfer(object):
             return "12345678"
 
     def _get_config(self):
+        """Download the apps.yml from the CDN."""
         http = urllib3.PoolManager()
         response = http.request('GET', self.url)
         this_dir = os.path.dirname(__file__)
@@ -70,39 +71,21 @@ class CDNTransfer(object):
             fhandle.close()
 
     def sync_config(self):
+        """Determines if the config file is updated and if so fetches the new config."""
         try:
+            logger.info("Config file is updated fetching new config.")
+            self._get_config()
+
             if self.is_updated():
-                logger.info("Config file is updated fetching new config.")
-                self._get_config()
                 # Touch app.py to force a gunicorn reload
                 self._touch()
                 return True
             else:
-                self._get_config()
                 return False
                 # Do nothing
         except Exception as e:
             print(e)
             logger.error("Problem fetching config file {error}".format(error=e))
-
-
-class DynamoTransfer(object):
-    """News up and does the job if configuration specifies that dynamo should be used for app information."""
-
-    def __init__(self, app_config):
-        self.configuration_table_name = "sso-dashboard-apps"
-        self.dynamodb = None
-
-    def connect_dynamodb(self):
-        if self.dynamodb is None:
-            dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
-            table = dynamodb.Table(self.configuration_table_name)
-            self.dynamodb = table
-
-    def sync_config(self):
-        self.connect_dynamodb()
-        results = self.dynamodb.scan(FilterExpression=Attr("name").exists())
-        return results.get("Items", None)
 
 
 class Tile(object):

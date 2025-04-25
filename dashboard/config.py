113e7f39aadc402cef6333bfe2f6cf393b6370bc
Bheesham Persaud <bhee@mozilla.com>
2025-04-24 20:36:25 -0400

    Environment variables were not shell friendly
    The environment names were dictated by some library we no longer use. I removed
    that dependency, but kept the environment variables the same to not break stuff
    anything.
    
    This commit changes that to be shell-friendly. Before, if you wanted to write a
    script to run the app, you'd need to resort to using either an external app, or
    `env -S $(< envfile) flask ...`.
    
    By swapping to dashes we're a little more generic in the kinds of ways you can
    call the app.
    
    Jira: IAM-950

--- dashboard/config.py
+++ dashboard/config.py
@@ -9,36 +9,41 @@ class Default:
     """Defaults for the configuration objects."""
 
     ENVIRONMENT: str = os.environ.get("ENVIRONMENT", "local")
-    DEBUG: bool = os.environ.get("SSO-DASHBOARD_DEBUG", "True") == "True"
-    TESTING: bool = os.environ.get("SSO-DASHBOARD_TESTING", "True") == "True"
 
-    CSRF_ENABLED: bool = os.environ.get("SSO-DASHBOARD_CSRF_ENABLED", "True") == "True"
-    PERMANENT_SESSION: bool = os.environ.get("SSO-DASHBOARD_PERMANENT_SESSION", "True") == "True"
+    # Flask makes use of the `FLASK_DEBUG` environment variable, with or
+    # without using `Flask.config.from_prefixed_env` (a method we don't
+    # use).
+    DEBUG: bool = os.environ.get("FLASK_DEBUG", "True") == "True"
+
+    TESTING: bool = os.environ.get("TESTING", "True") == "True"
+
+    CSRF_ENABLED: bool = os.environ.get("CSRF_ENABLED", "True") == "True"
+    PERMANENT_SESSION: bool = os.environ.get("PERMANENT_SESSION", "True") == "True"
     PERMANENT_SESSION_LIFETIME: datetime.timedelta = datetime.timedelta(
-        seconds=int(os.environ.get("SSO-DASHBOARD_PERMANENT_SESSION_LIFETIME", "86400"))
+        seconds=int(os.environ.get("PERMANENT_SESSION_LIFETIME", "86400"))
     )
 
-    SESSION_COOKIE_SAMESITE: str = os.environ.get("SSO-DASHBOARD_SESSION_COOKIE_SAMESITE", "lax")
-    SESSION_COOKIE_HTTPONLY: bool = os.environ.get("SSO-DASHBOARD_SESSION_COOKIE_HTTPONLY", "True") == "True"
+    SESSION_COOKIE_SAMESITE: str = os.environ.get("SESSION_COOKIE_SAMESITE", "lax")
+    SESSION_COOKIE_HTTPONLY: bool = os.environ.get("SESSION_COOKIE_HTTPONLY", "True") == "True"
 
     SECRET_KEY: str
-    SERVER_NAME: str = os.environ.get("SSO-DASHBOARD_SERVER_NAME", "localhost:8000")
+    SERVER_NAME: str = os.environ.get("SERVER_NAME", "localhost:8000")
     SESSION_COOKIE_NAME: str
     CDN: str
 
     S3_BUCKET: str
     FORBIDDEN_PAGE_PUBLIC_KEY: bytes
 
-    PREFERRED_URL_SCHEME: str = os.environ.get("SSO-DASHBOARD_PREFERRED_URL_SCHEME", "https")
+    PREFERRED_URL_SCHEME: str = os.environ.get("PREFERRED_URL_SCHEME", "https")
     REDIS_CONNECTOR: str
 
     def __init__(self):
         self.SESSION_COOKIE_NAME = f"{self.SERVER_NAME}_session"
-        self.CDN = os.environ.get("SSO-DASHBOARD_CDN", f"https://cdn.{self.SERVER_NAME}")
-        self.REDIS_CONNECTOR = os.environ["SSO-DASHBOARD_REDIS_CONNECTOR"]
-        self.SECRET_KEY = os.environ["SSO-DASHBOARD_SECRET_KEY"]
-        self.S3_BUCKET = os.environ["SSO-DASHBOARD_S3_BUCKET"]
-        self.FORBIDDEN_PAGE_PUBLIC_KEY = base64.b64decode(os.environ["SSO-DASHBOARD_FORBIDDEN_PAGE_PUBLIC_KEY"])
+        self.CDN = os.environ.get("CDN", f"https://cdn.{self.SERVER_NAME}")
+        self.REDIS_CONNECTOR = os.environ["REDIS_CONNECTOR"]
+        self.SECRET_KEY = os.environ["SECRET_KEY"]
+        self.S3_BUCKET = os.environ["S3_BUCKET"]
+        self.FORBIDDEN_PAGE_PUBLIC_KEY = base64.b64decode(os.environ["FORBIDDEN_PAGE_PUBLIC_KEY"])
 
 
 class OIDC:
@@ -52,12 +57,12 @@ class OIDC:
 
     def __init__(self):
         """General object initializer."""
-        self.OIDC_DOMAIN = os.environ["SSO-DASHBOARD_OIDC_DOMAIN"]
-        self.OIDC_CLIENT_ID = os.environ["SSO-DASHBOARD_OIDC_CLIENT_ID"]
-        self.OIDC_CLIENT_SECRET = os.environ["SSO-DASHBOARD_OIDC_CLIENT_SECRET"]
+        self.OIDC_DOMAIN = os.environ["OIDC_DOMAIN"]
+        self.OIDC_CLIENT_ID = os.environ["OIDC_CLIENT_ID"]
+        self.OIDC_CLIENT_SECRET = os.environ["OIDC_CLIENT_SECRET"]
         # Check for a prefixed environment variable, otherwise fallback to the
         # unprefixed one.
-        if redirect_uri := os.environ.get("SSO-DASHBOARD_OIDC_REDIRECT_URI"):
+        if redirect_uri := os.environ.get("OIDC_REDIRECT_URI"):
             self.OIDC_REDIRECT_URI = redirect_uri
         else:
             self.OIDC_REDIRECT_URI = os.environ["OIDC_REDIRECT_URI"]
